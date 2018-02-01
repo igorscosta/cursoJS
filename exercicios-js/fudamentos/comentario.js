9ef7d381ee51277b0bd1ba5afc1b77ab223b660b
Igor Costa <igorsc93@gmail.com>
2018-02-01 03:56:12 -0200

    Exercicio sobre comentarios

---
+++ exercicios-js/fudamentos/comentario.js
@@ -0,0 +1,15 @@
+//Comentário de uma lina
+console.log('Linha 1')
+
+/*
+ Comentário de 
+ multiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+ * Comentário de
+ * Múltiplas Linhas
+ */
+console.log('Linha 3')
+

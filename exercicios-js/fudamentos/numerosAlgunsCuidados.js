089770cfcce3d3bcef02204f03cd4f97c8c00e56
Igor Costa <igorsc93@gmail.com>
2018-02-01 05:01:17 -0200

    Alguns cuidados com tipos numericos em JS

---
+++ exercicios-js/fudamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(42 / 0) // retorna valor infinito 
+console.log(typeof Infinity)
+console.log("10" / 2) // retorna 5
+console.log("Igor" * 2) // retorna NaN
+console.log(0.1 + 0.7) // retorn 0.7999999999
+console.log((10).toString())

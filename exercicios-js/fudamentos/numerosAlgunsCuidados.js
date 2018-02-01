a264377149fd89ce999a7788c17897f312e984f8
Igor Costa <igorsc93@gmail.com>
2018-02-01 05:02:09 -0200

    Alguns cuidados com tipos numericos em JS

--- exercicios-js/fudamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fudamentos/numerosAlgunsCuidados.js
@@ -3,4 +3,6 @@ console.log(typeof Infinity)
 console.log("10" / 2) // retorna 5
 console.log("Igor" * 2) // retorna NaN
 console.log(0.1 + 0.7) // retorn 0.7999999999
-console.log((10).toString())
+console.log(10.toString())// não funciona 
+console.log((10).toString())// funciona
+console.log((10.354).toFixed(2))// funciona, pois a chamada de função para um litral direto não funciona, mas dentro de () funciona

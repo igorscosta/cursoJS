a516ac7512ffaafbbf133d78efe08fd9b6df0802
Igor Costa <igorsc93@gmail.com>
2018-02-01 06:38:20 -0200

    Aula sobre strings

--- exercicios-js/fudamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fudamentos/numerosAlgunsCuidados.js
@@ -6,3 +6,4 @@ console.log(0.1 + 0.7) // retorn 0.7999999999
 console.log(10.toString())// não funciona 
 console.log((10).toString())// funciona
 console.log((10.354).toFixed(2))// funciona, pois a chamada de função para um litral direto não funciona, mas dentro de () funciona
+console.log('3'+ 2) //concate pois string tem preferencia
---
+++ exercicios-js/fudamentos/strings.js
@@ -0,0 +1,15 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5)) // retorna vazio
+console.log(escola.charCodeAt(3)) // retorna o valor unicode
+console.log(escola.indexOf('o')) // retorna o indice de um caráctere
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3)) // Vai do indice 0 ao 3 sem incluir o 3
+
+console.log('Escola '.concat(escola).concat("!")) //concatenação de strings
+console.log('Escola ' + escola + "!") //outra forma de concaternar strings
+console.log(escola.replace(3, 'e')) // substituição de caracteres
+
+console.log('Igu,Maria,Jão'.split(',')) //pegando elementos de uma string e jogando em um array, aceita regex

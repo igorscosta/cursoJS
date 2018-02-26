66875bad92fe9f2f49b18a4b912ee7eb8df82f4a
Igor Costa <igorsc93@gmail.com>
2018-02-26 17:03:29 -0300

    Aula sobre funções e objetos

---
+++ exercicios-js/fudamentos/objeto2.js
@@ -0,0 +1,17 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+//class Produto {} // ES 2015 (ES6)
+//console.log(typeof Produto)
+//console.log(typeof new Produto())
+
+//EM JS é possivel instanciar uma função, ou seja uma função exerce o papel de uma classe.
+//Funções podem ser instaciandas através da palavra reservada new
+//
+
+Produto.igu = 'igu'
+console.log(Produto.igu)

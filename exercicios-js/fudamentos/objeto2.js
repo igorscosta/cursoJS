f71a2b774930127864d354d37b73d9215af3ecda
Igor Costa <igorsc93@gmail.com>
2018-02-27 23:33:45 -0300

    ...

--- exercicios-js/fudamentos/objeto2.js
+++ exercicios-js/fudamentos/objeto2.js
@@ -5,13 +5,11 @@ const Cliente = function() {}
 console.log(typeof Cliente)
 console.log(typeof new Cliente)
 
-//class Produto {} // ES 2015 (ES6)
-//console.log(typeof Produto)
-//console.log(typeof new Produto())
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
 
 //EM JS é possivel instanciar uma função, ou seja uma função exerce o papel de uma classe.
 //Funções podem ser instaciandas através da palavra reservada new
-//
-
-Produto.igu = 'igu'
-console.log(Produto.igu)
+//A utilização da palavra class é somente um açúcar sintático, internamente é criada uma função
+//Funçõe em JS podem ter métodos e atributos, essas funções podem ser instânciadas 

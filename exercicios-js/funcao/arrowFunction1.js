8c73f255ac08996276cb484d78642a38b48c037e
Igor Costa <igorsc93@gmail.com>
2018-03-24 15:59:18 -0300

    Função Arrow 1

---
+++ exercicios-js/funcao/arrowFunction1.js
@@ -0,0 +1,22 @@
+let dobro = function(a){
+    return 2 * a
+}
+
+//Função arrow é sempre uma função anônima
+//Caso deseje chamá-la depois, deve-se armazena-la em uma
+//constante ou variável
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a //return implícito
+console.log(dobro(Math.PI))
+
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+ola = _ => 'Olá' // possui um param
+console.log(ola())

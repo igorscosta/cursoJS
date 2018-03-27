d3656b7d1a02d75456ee6f3e7e63b8923284a8d2
Igor Costa <igorsc93@gmail.com>
2018-03-26 22:27:26 -0300

    Funções callback

---
+++ exercicios-js/funcao/callback1.js
@@ -0,0 +1,8 @@
+const fabricantes = ["Mercedes", "Audi", "BWM"]
+
+function imprimir(nome, indice){
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))

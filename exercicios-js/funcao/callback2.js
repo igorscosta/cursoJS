e6ce35580138f2bf0e504e81313b01064aeb323f
Igor Costa <igorsc93@gmail.com>
2018-04-03 22:16:10 -0300

    Funções Callback 2

--- exercicios-js/funcao/callback2.js
+++ exercicios-js/funcao/callback2.js
@@ -1,1 +1,24 @@
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+// Sem callback
+const notasBaixas1 = []
+for (let i in notas) {
+    if (notas[i] <= 7) {
+        notasBaixas1.push(notas[i])
+    }
+}
+
+console.log(notasBaixas1)
+
+// Com callback
+const notasBaixas2 = notas.filter(nota => nota <= 7)
+console.log(notasBaixas2)
+
+const notasBaixas3 = notas.filter(function (nota) {
+    return nota <= 7
+})
+console.log(notasBaixas3)
+
+const notasMenorOuIgualA7 = nota => nota <= 7
+const notasBaixas4 = notas.filter(notasMenorOuIgualA7)
+console.log(notasBaixas4)
